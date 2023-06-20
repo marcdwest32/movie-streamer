1d0e49236a2c8b0f0637be14e3633afaa8f63caf
Your marcdwest32 <marcdwest@gmail.com>
2023-06-20 15:47:24 -0500

    fetch data from youtube api

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,16 @@ import CardSection from './components/cardSection/cardSection'
 import styles from './page.module.css'
 import { getVideos } from './lib/videos'
 
-export default function Home() {
-  const videos = getVideos()
+const defaultImg =
+  'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+
+// This gets called on every request
+async function getData() {
+  const data = await getVideos() // The return value is *not* serialized
+  return data
+}
+
+export default async function Home() {
   const disneyVideos = [
     {
       imgUrl: '/static/clifford.webp',
@@ -29,15 +37,12 @@ export default function Home() {
       imgUrl: '/static/clifford.webp',
     },
   ]
+  const videos = (await getData()) || []
 
   return (
     <main>
       <Navbar username='Marc@gmail.com' />
-      <Banner
-        title='Clifford the Big Red Dog'
-        subTitle='big red dog'
-        imgUrl='/static/clifford.webp'
-      />
+      <Banner title='MeTube' subTitle='projector' imgUrl={defaultImg} />
       <div className={styles.sectionWrapper}>
         <CardSection title='Space' videos={videos} size='large' />
         <CardSection title='Disney' videos={disneyVideos} size='medium' />
