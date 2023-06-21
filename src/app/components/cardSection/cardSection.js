b86025fa2f2ab585be2f5cde9b59ce3b5007b4d9
Your marcdwest32 <marcdwest@gmail.com>
2023-06-21 17:56:46 -0500

    remove unused code

--- src/app/components/cardSection/cardSection.js
+++ src/app/components/cardSection/cardSection.js
@@ -3,12 +3,8 @@ import { useRef } from 'react'
 import Image from 'next/image'
 import Card from '../card/card'
 import styles from './cardSection.module.css'
-// import clifford from '../../../../public/static/clifford.webp'
-// const defaultImg =
-//   'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
 
 const CardSection = ({ size, title, videos }) => {
-  //   console.log({ videos })
   const cardWrapperRef = useRef()
 
   const scrollRight = () => {
@@ -39,7 +35,6 @@ const CardSection = ({ size, title, videos }) => {
       </div>
       <div className={styles.cardWrapper} ref={cardWrapperRef}>
         {videos.map((video, i) => {
-          //   console.log(video)
           return <Card imgUrl={video.imgUrl} size={size} id={i} key={i} />
         })}
       </div>
