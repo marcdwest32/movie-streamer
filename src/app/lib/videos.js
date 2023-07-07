a10d59954381f80601fed1ed0c980ac6aab23e76
Your marcdwest32 <marcdwest@gmail.com>
2023-07-07 17:56:31 -0500

    use dummy data instead of api calls for testing

--- src/app/lib/videos.js
+++ src/app/lib/videos.js
@@ -1,33 +1,46 @@
-export const getCommonVideos = async (url) => {
-  const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY
-  const baseUrl = 'https://youtube.googleapis.com/youtube/v3/'
+// export const getCommonVideos = async (url) => {
+//   const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY
+//   const baseUrl = 'https://youtube.googleapis.com/youtube/v3/'
 
-  const response = await fetch(`${baseUrl}${url}&key=${YOUTUBE_API_KEY}`)
+//   const response = await fetch(`${baseUrl}${url}&key=${YOUTUBE_API_KEY}`)
 
-  const data = await response.json()
+//   const data = await response.json()
 
-  if (data?.error) {
-    console.error('Something went wrong with the video library', data.error)
-    return []
-  }
+//   if (data?.error) {
+//     console.error('Something went wrong with the video library', data.error)
+//     return []
+//   }
 
-  return data.items.map((item) => {
-    // console.log(item)
-    const id = item.id?.videoId || item.id
+//   return data.items.map((item) => {
+//     // console.log(item)
+//     const id = item.id?.videoId || item.id
+//     return {
+//       title: item.snippet.title,
+//       imgUrl: item.snippet.thumbnails.high.url,
+//       id,
+//     }
+//   })
+// }
+
+// export const getPopularVideos = async () => {
+//   const url = `videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US`
+//   return getCommonVideos(url)
+// }
+
+// export const getVideos = (searchQuery) => {
+//   const url = `search?part=snippet&type=video&maxResults=25&q=${searchQuery}`
+//   return getCommonVideos(url)
+// }
+
+// Testing
+import videoData from '../data/videos.json'
+
+export const getVideos = (category) => {
+  return videoData[category].items.map((item) => {
     return {
       title: item.snippet.title,
       imgUrl: item.snippet.thumbnails.high.url,
-      id,
+      id: item?.id?.videoId,
     }
   })
-}
-
-export const getPopularVideos = async () => {
-  const url = `videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=25&regionCode=US`
-  return getCommonVideos(url)
-}
-
-export const getVideos = (searchQuery) => {
-  const url = `search?part=snippet&type=video&maxResults=25&q=${searchQuery}`
-  return getCommonVideos(url)
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+'use client'
+import { useEffect, useState } from 'react'
 import Navbar from './components/nav/navbar'
 import Banner from './components/banner/banner'
 import CardSection from './components/cardSection/cardSection'
@@ -7,37 +9,78 @@ import { getVideos, getPopularVideos } from './lib/videos'
 const defaultImg =
   'https://images.unsplash.com/photo-1535016120720-40c646be5580?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
 
+// production
 // This gets called on every request
+// async function getData() {
+//   const kurzgesagtVideos = await getVideos('kurzgesagt')
+//   const marvelVideos = await getVideos('marvel comics')
+//   const nflVideos = await getVideos('nfl')
+//   const popularVideos = await getPopularVideos()
+//   return {
+//     results: { kurzgesagtVideos, marvelVideos, nflVideos, popularVideos },
+//   }
+// }
+
+// Testing
 async function getData() {
-  const kurzgesagtVideos = await getVideos('kurzgesagt')
-  const marvelVideos = await getVideos('marvel comics')
-  const nflVideos = await getVideos('nfl')
-  const popularVideos = await getPopularVideos()
+  const kurzgesagtVideos = getVideos('kurzgesagt')
+  const marvelVideos = getVideos('marvel')
+  const nflVideos = getVideos('nfl')
+  const spaceVideos = getVideos('space')
   return {
-    results: { kurzgesagtVideos, marvelVideos, nflVideos, popularVideos },
+    results: {
+      kurzgesagtVideos,
+      marvelVideos,
+      nflVideos,
+      spaceVideos,
+    },
   }
 }
 
-export default async function Home() {
-  const videos = (await getData()) || []
-  console.log(videos)
-  const { kurzgesagtVideos, marvelVideos, nflVideos, popularVideos } =
-    videos.results
+export default function Home() {
+  //   const [didToken, setDidToken] = useState(null)
+  //   const videos = (await getData()) || []
+  //   const { kurzgesagtVideos, marvelVideos, nflVideos, spaceVideos } =
+  //     videos.results
+
+  //   useEffect(() => {
+  //     // Check if the didToken exists and set it in the context
+  //     if (didToken) {
+  //       setDidToken(didToken)
+  //     }
+  //   }, [didToken])
+  const [videos, setVideos] = useState(null)
+
+  useEffect(() => {
+    // Function to fetch data asynchronously
+    async function fetchData() {
+      const data = await getData()
+      setVideos(data.results)
+    }
+
+    fetchData()
+  }, [])
+
+  if (!videos) {
+    return <div>Loading...</div>
+  }
+
+  const { kurzgesagtVideos, marvelVideos, nflVideos, spaceVideos } = videos
 
   return (
-    <main className={styles.main}>
-      <Navbar username='Marc@gmail.com' />
-      <Banner title='MeTube' subTitle='projector' imgUrl={defaultImg} />
-      <div className={styles.sectionWrapper}>
-        <CardSection title='Marvel' videos={marvelVideos} size='large' />
-        <CardSection
-          title='Kurzgesagt'
-          videos={kurzgesagtVideos}
-          size='small'
-        />
-        <CardSection title='NFL' videos={nflVideos} size='small' />
-        <CardSection title='Popular' videos={popularVideos} size='medium' />
-      </div>
-    </main>
+      <main className={styles.main}>
+        <Navbar username='Marc@gmail.com' />
+        <Banner title='MeTube' subTitle='projector' imgUrl={defaultImg} />
+        <div className={styles.sectionWrapper}>
+          <CardSection title='Marvel' videos={marvelVideos} size='large' />
+          <CardSection
+            title='Kurzgesagt'
+            videos={kurzgesagtVideos}
+            size='small'
+          />
+          <CardSection title='NFL' videos={nflVideos} size='small' />
+          <CardSection title='Popular' videos={spaceVideos} size='medium' />
+        </div>
+      </main>
   )
 }
