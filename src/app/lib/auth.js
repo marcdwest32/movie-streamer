cb6583df485a8b4a241d75fd22c2ff71a904bdf3
Your marcdwest32 <marcdwest@gmail.com>
2023-07-07 17:57:10 -0500

    add nextauth

---
+++ src/app/lib/auth.js
@@ -0,0 +1,33 @@
+const { NextAuthOptions } = require('next-auth')
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const authOptions = {
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'example@example.com',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+        },
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials
+        console.log(credentials)
+        const username = email.split('@')[0]
+        const user = { id: '1', name: username, email }
+        return user
+      },
+    }),
+  ],
+}
+
+module.exports = { authOptions }
