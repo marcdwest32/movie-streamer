7782b975c5932e814e27f028294ad6387925b390
Your marcdwest32 <marcdwest@gmail.com>
2023-08-08 13:22:17 -0500

    video modal styling

--- src/app/video/[videoId]/page.js
+++ src/app/video/[videoId]/page.js
@@ -1,25 +1,65 @@
 'use client'
 import { useRouter, useSearchParams, usePathname } from 'next/navigation'
 import Modal from 'react-modal'
+import cls from 'classnames'
 import styles from './video.module.css'
 Modal.setAppElement('#__next')
 
 const Video = () => {
   const router = useRouter()
   const fullPath = usePathname()
-  const pathname = fullPath.split('video/')[1]
-  console.log(pathname)
-  //   console.log({ router })
+  const videoId = fullPath.split('video/')[1]
+
+  const video = {
+    title: 'title',
+    publishTime: '1990-01-01',
+    description:
+      'A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?',
+    channelTitle: 'Picture Company',
+    viewCount: 100000,
+  }
+
+  const { title, publishTime, description, channelTitle, viewCount } = video
+
   return (
-    <div>
-      video page {pathname}
+    <div className={styles.container}>
+      video page {videoId}
       <Modal
         isOpen={true}
         contentLabel='Watch Video'
         onRequestClose={() => router.back()}
+        className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <div>Modal Body</div>
+        <iframe
+          id='player'
+          className={styles.videoPlayer}
+          type='text/html'
+          width='100%'
+          height='390'
+          src={`http://www.youtube.com/embed/${videoId}?enablejsapi=1&origin=http://example.com&controls=0&rel=0`}
+          frameborder='0'
+        ></iframe>
+
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={cls(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   )
